07dacb4ff280f28774a55416a75caa0790f989b8
Robert <kermitsrocket@gmail.com>
2025-01-17 12:43:31 -0600

    Enhanced logging configuration: Added console logging with detailed format, updated logger name to 'ematac_logger', and improved debugging capabilities with file, line, and function info.

--- modules/configuration/log_config.py
+++ modules/configuration/log_config.py
@@ -13,19 +13,28 @@ else:
 sys.path.append(BASE_DIR)
 
 # Configure logging
-logger = logging.getLogger('app_logger')
+logger = logging.getLogger('ematac_logger')
 logger.setLevel(logging.DEBUG)
 
 # Create a file handler
 file_handler = logging.FileHandler('../../app.log')
 file_handler.setLevel(logging.DEBUG)
 
-# Create a formatter and set it for the handler
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Create a console handler
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+
+# Create a formatter and set it for both handlers
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s() - %(message)s'
+)
+
 file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 
-# Add the file handler to the logger
+# Add both handlers to the logger
 logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
-# Remove the default StreamHandler to disable logging to the terminal
+# Optionally, prevent log messages from being propagated to the root logger
 logger.propagate = False
